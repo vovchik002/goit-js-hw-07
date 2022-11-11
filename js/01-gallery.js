b0b825b6d0ffaa2e1772170617f0042faaab37cd
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-12 00:46:47 +0200

    1done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const imgAdd = `<div class="gallery__item">
-<a class="gallery__link" href="large-image.jpg">
-  <img
-    class="gallery__image"
-    src="small-image.jpg"
-    data-source="large-image.jpg"
-    alt="Image description"
-  />
-</a>
-</div>`
+const containerImg = document.querySelector(".gallery");
+const imgCreateInHtml = createImgGallery(galleryItems);
+containerImg.insertAdjacentHTML("beforeend", imgCreateInHtml);
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+ <a class="gallery__link" href="${original}">
+   <img
+     class="gallery__image"
+     src="${preview}"
+     data-source="${original}" 
+     alt="${description}"
+   />
+ </a>
+ </div>`;
+    })
+    .join("");
+}
 
-console.log(galleryItems);
+containerImg.addEventListener('click', onContainerImgClick);
+
+function onContainerImgClick(event) {
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+    instance.show();
+
+
+    addEventListener('keydown', event => {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    })
+}
