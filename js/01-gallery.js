9a2f59c0e59936800cd9fd7eee0497a32568bf43
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-11 23:05:30 +0200

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,14 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const imgAdd = `<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="small-image.jpg"
+    data-source="large-image.jpg"
+    alt="Image description"
+  />
+</a>
+</div>`
+
+console.log(galleryItems);
