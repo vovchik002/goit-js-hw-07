69395453562c8c860d011d8e944b7fa85a6ac409
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-14 14:59:38 +0200

    2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerImg = document.querySelector(".gallery");
+const imgCreateInHtml = createImgGallery(galleryItems);
+containerImg.insertAdjacentHTML("beforeend", imgCreateInHtml);
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+containerImg.addEventListener('click', onContainerImgClick);
 
-console.log(galleryItems);
+function onContainerImgClick(event) {
+    event.preventDefault();
+}
+
+    var lightbox = new SimpleLightbox('ul.gallery a', { captionsData: 'alt', captionsDelay: 250})
+
